2c5fbb7705a8285618eb1b9dad0938c19cbe8776
philip howley <howley.phil@gmail.com>
2020-12-22 15:46:19 -0500

    Add form with handler and submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,58 @@
+import { useState } from "react";
+
 function ContactForm() {
-  <section>
-    <h1>Contact Me</h1>
-    <form id="contact-form">
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
-      </div>
-      <div>
-        <label htmlFor="email">Email address:</label>
-        <input type="text" name="email" />
-      </div>
-      <div>
-        <label htmlFor="">Message:</label>
-        <textarea name="messgage" rows="5" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </section>;
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="text"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Message:</label>
+          <textarea
+            name="messgage"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
