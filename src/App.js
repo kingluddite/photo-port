11ec99366bd9869d528f0b3c62779a8574f298cd
philip howley <howley.phil@gmail.com>
2020-12-22 01:47:11 -0500

    Add tests for PhotoList, Gallery and Nav

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       />
       <main>
         <div>
-          <Gallery />
+          <Gallery currentCategory={currentCategory} />
           <About />
         </div>
       </main>
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,23 +1,14 @@
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import photo from "../../assets/small/commercial/0.jpg";
+import PhotoList from "../PhotoList";
 
 function Gallery(props) {
-  const currentCategory = {
-    name: "commercial",
-    description: "desc of commerical",
-  };
+  const { currentCategory } = props;
 
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.description}</p>
-      <div className="flex-row">
-        <img
-          className="img-thumbnail mx-1"
-          src={photo}
-          alt="Commercial Example"
-        />
-      </div>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
